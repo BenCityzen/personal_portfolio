6bf508fd1f868d2b3e64cb94a3417a84d6d6ad89
BenCityzen <thulanibmoloi@gmail.com>
2025-04-12 23:32:44 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,8 @@ let header = $(`
   
      <li class="nav-item nav-item-hover">
 
-<a href="files/TBMoloi.pdf" target="_blank">View PDF</a>
+<embed src="files/TBMoloi.pdf" type="application/pdf" width="100%" height="600px">
+
 
 
 
