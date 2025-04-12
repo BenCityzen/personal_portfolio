50eed15e34504ed12ac07737ddb0cbea54a2cc0e
BenCityzen <thulanibmoloi@gmail.com>
2025-04-12 23:51:53 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,8 @@ let header = $(`
   
      <li class="nav-item nav-item-hover">
 
-<iframe src="https://mozilla.github.io/pdf.js/web/viewer.html?file=TBMoloi.pdf" width="100%" height="600px"></iframe>
+<a href="assets/pdfs/TBMoloi.pdf" target="_blank">Open PDF</a>
+
 
 
 </li>
